8d93c085effa12381e7a2dd7383a325f61b4a737
Patrick Santos <patrickspteixeira@gmail.com>
2019-10-26 21:04:51 -0300

    adiciona estilos e lib de icones

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Form, Input, SubmitButton } from './styles';
 
 const Main = () => {
-    return <Container />;
+    return (
+        <Container>
+            <Form>
+                <Input
+                    autoCorrect={false}
+                    autoCapitalize="none"
+                    placeholder="Adicionar usuário"
+                />
+                <SubmitButton>
+                    <Icon name="add" size={20} color="#fff" />
+                </SubmitButton>
+            </Form>
+        </Container>
+    );
 };
 
 Main.navigationOptions = {
